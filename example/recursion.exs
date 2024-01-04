6d313c8f0e0ee82c36bfcaadd1eb86a555b82acf
NANTHA2001 <nanthakumar.g@bigthinkcode.com>
2024-01-04 12:10:08 +0530

    updated code

---
+++ example/recursion.exs
@@ -0,0 +1,15 @@
+# recursion adding head alone
+defmodule Recursion do
+  def fun([]), do: 0
+  def fun([head |tail]), do: head + fun(tail)
+end
+
+# addind sum of the list
+defmodule AddingSum do
+  def sum([]), do: 0
+  def sum([ _ |tail]), do: 1 + sum(tail)
+end
+
+
+IO.puts(Recursion.fun([1,2,3]))
+IO.puts AddingSum.sum([1,2,3])
