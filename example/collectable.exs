08a636f76a7c9f21ffde353ceb56179a3eda667a
NANTHA2001 <nanthakumar.g@bigthinkcode.com>
2024-01-09 10:55:42 +0530

    updated code

---
+++ example/collectable.exs
@@ -0,0 +1,20 @@
+defimpl Collectable, for: MapSet do
+  def into(map_set) do
+    collector_fun = fn
+      map_set_acc, {:cont, elem} ->
+        MapSet.put(map_set_acc, elem)
+
+      map_set_acc, :done ->
+        map_set_acc
+
+      map_set_acc, :halt ->
+        :ok
+    end
+
+    initial_acc = map_set
+
+    {initial_acc, collector_fun}
+  end
+end
+
+IO.inspect Enum.into([1, 2, 3], MapSet.new())
